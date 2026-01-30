fe043804df83802d3c5078d126fc4e46b30a454d
Giri Putra Adhittana <giri.adhittana@bpsdata.com>
2026-01-30 11:34:08 +0700

    fix: adjustment

--- middleware/cors.go
+++ middleware/cors.go
@@ -8,29 +8,8 @@ import (
 
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		origin := c.Request.Header.Get("Origin")
-
-		allowedOrigins := []string{
-			"http://localhost:3000",
-			"http://localhost:3001",
-			"http://localhost:5173",
-		}
-
-		isAllowed := false
-		for _, allowedOrigin := range allowedOrigins {
-			if origin == allowedOrigin {
-				isAllowed = true
-				break
-			}
-		}
-
-		if isAllowed {
-			c.Header("Access-Control-Allow-Origin", origin)
-		} else if len(allowedOrigins) > 0 {
-			c.Header("Access-Control-Allow-Origin", allowedOrigins[0])
-		}
-
-		c.Header("Access-Control-Allow-Credentials", "true")
+		// Allow all origins
+		c.Header("Access-Control-Allow-Origin", "*")
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, Stripe-Signature")
 		c.Header("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
 
